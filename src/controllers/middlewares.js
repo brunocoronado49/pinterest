2ea637f3bc15c7d4634e21639dcffc69cd9f0812
brunocoronado49 <jofrancoracor@gmail.com>
2022-02-15 13:39:56 -0600

    Configuracion terminada para subir imagenes

--- src/controllers/middlewares.js
+++ src/controllers/middlewares.js
@@ -6,7 +6,7 @@ import { v4 as uuid } from 'uuid';
 const storage = multer.diskStorage({
     destination: path.join(__dirname, './public/img/uploads'),
     filename: (req, file, cb) => {
-
+        cb(null, uuid() + path.extname(file.originalname).toLocaleLowerCase());
     }
 });
 
@@ -14,5 +14,15 @@ export const upload = multer({
     dest: path.join(__dirname, './public/img/uploads'),
     storage: storage,
     limits: { fileSize: 3000000 },
-    fileFilter: (req, file, cb) => {},
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png/;
+        const mimetype = fileTypes.test(file.mimetype);
+        const extname = fileTypes.test(path.extname(file.originalname));
+
+        if(mimetype && extname) {
+            return cb(null, true);
+        }
+
+        cb("Error al subir la imagen");
+    },
 }).single('image');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,32 @@
 import { Router } from 'express';
+import { upload } from '../controllers/middlewares.js';
 
 const router = Router();
 
+// Pagina de inicio
 router.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('index');
+});
+
+// perfil de una imagen
+router.get('/image/:id', (req, res) => {
+    res.send('image profile');
+});
+
+// mostrar el formulario para subir imagen
+router.get('/upload', (req, res) => {
+    res.render('upload');
+});
+
+// metodo para subir imagen
+router.post('/upload', upload,(req, res) => {
+    console.log(req.file);
+    res.send('uploaded');
+});
+
+// eliminar una imagen
+router.get('/delete/:id', (req, res) => {
+    res.send('image deleted');
 });
 
 export default router;
